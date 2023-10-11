70c1bb102f363c1e760e2563f9b2ccf850b4aab3
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2023-10-11 11:09:34 +0200

    🐛 FIX: return type

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -6,11 +6,12 @@ namespace Aih\AihBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class HealthCheckController extends AbstractController
 {
     #[Route('/healthcheck', name: 'healthcheck')]
-    public function index()
+    public function index(): Response
     {
         return $this->json([
             'status' => 'L\'Application est en ligne',
