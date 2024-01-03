920f9ad237bef1e18fcad81e19eb7019a157a6a7
Yoan Bernabeu <yoan.bernabeu@gmail.com>
2024-01-03 11:51:44 +0100

    🐛 FIX: règles php cs

--- src/Service/CmtApiInterface.php
+++ src/Service/CmtApiInterface.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 namespace Aih\AihBundle\Service;
 
-interface CmtApiInterface extends GraphQlClientInterface {}
+interface CmtApiInterface extends GraphQlClientInterface
+{
+}
