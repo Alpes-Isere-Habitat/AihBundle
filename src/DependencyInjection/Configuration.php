15b51da1144397d5166df6db6494765e0dd7f4c8
benoit <benoit.barberot@alpeshabitat.fr>
2025-02-27 17:32:12 +0100

    📦 NEW: Tous les paramètre nullables

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,166 +15,175 @@ class Configuration implements ConfigurationInterface
         $treeBuilder->getRootNode()
             ->children()
                 ->scalarNode('HAPPLYAPI_USER')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un compte pour HapplyApi.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYAPI_PASSWORD')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un mot de passe pour HapplyApi.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYAPI_URL')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'URL de l\'API de HapplyApi.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYAPIGD_USER')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un compte pour HapplyApiGD.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYAPIGD_PASSWORD')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un mot de passe pour HapplyApiGD.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYAPIGD_URL')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'URL de l\'API de HapplyApiGD.'
                     )
                 ->end()
                 ->scalarNode('CMTAPI_USER')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un compte l\'API de CMT.'
                     )
                 ->end()
                 ->scalarNode('CMTAPI_PASSWORD')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un mot de passe l\'API de CMT.'
                     )
                 ->end()
                 ->scalarNode('CMTAPI_URL')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'URL de l\'API de HapplyApi.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYHAB_APPLICATION')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'application pour laquelle vous souhaitez utiliser HapplyHab.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYHAB_USER')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un compte pour HapplyHab.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYHAB_PASSWORD')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un mot de passe pour HapplyHab.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYHAB_URL')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'URL de l\'API de HapplyHab.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYSMS_URL')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'URL de l\'API de HapplySms.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYSMS_USER')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un compte pour HapplySms.'
                     )
                 ->end()
                 ->scalarNode('HAPPLYSMS_PASSWORD')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier un mot de passe pour HapplySms.'
                     )
                 ->end()
                 ->scalarNode('AZURE_TENANT_ID')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'ID de l\'organisation Azure.'
                     )
                 ->end()
                 ->scalarNode('AZURE_CLIENT_ID')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'ID du client Azure.'
                     )
                 ->end()
                 ->scalarNode('AZURE_CLIENT_SECRET')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier le secret du client Azure.'
                     )
                 ->end()
                 ->scalarNode('AZURE_GROUP_ALL_USER')
-                    ->isRequired()
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier le nom du groupe d\'utilisateurs Azure.'
                     )
                 ->end()
                 ->scalarNode('BUCKET_BACKUP_NAME')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier le nom du bucket de sauvegarde.'
                     )
                 ->end()
                 ->scalarNode('BUCKET_BACKUP_ACCESS_KEY')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier la clé d\'accès du bucket de sauvegarde.'
                     )
                 ->end()
                 ->scalarNode('BUCKET_BACKUP_SECRET_KEY')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier le secret de la clé d\'accès du bucket de sauvegarde.'
                     )
                 ->end()
                 ->scalarNode('BUCKET_PATH')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier le chemin du bucket de sauvegarde.'
                     )
                 ->end()
                 ->scalarNode('DATABASE_TYPE')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier le type de base de données.'
                     )
                 ->end()
                 ->scalarNode('DATABASE_USER')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'utilisateur de la base de données.'
                     )
                 ->end()
                 ->scalarNode('DATABASE_NAME')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier le nom de la base de données.'
                     )
                 ->end()
                 ->scalarNode('CONTAINER_NAME')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier le nom du container Docker.'
                     )
                 ->end()
                 ->scalarNode('BUCKET_ENDPOINT')
+                    ->defaultNull()
                     ->info(
                         'Vous devez spécifier l\'URL de l\'endpoint S3.'
                     )
