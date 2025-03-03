a47eb87ccf696b698114743d2cbfe65ee45e1916
benoit <benoit.barberot@alpeshabitat.fr>
2025-03-03 10:45:36 +0100

    🐛 FIX: dead code

--- src/Service/HapplyHab.php
+++ src/Service/HapplyHab.php
@@ -8,11 +8,6 @@ use Exception;
 
 class HapplyHab extends AbstractHapply implements HapplyHabInterface
 {
-    private string $serviceContainerUserParameter = 'aih_aih.happlyhab.user';
-    private string $serviceContainerPasswordParameter = 'aih_aih.happlyhab.password';
-    private string $serviceContainerUrlParameter = 'aih_aih.happlyhab.url';
-    private string $serviceContainerApplicationParameter = 'aih_aih.happlyhab.application';
-
     protected array $requiredParameters = [
         'aih_aih.happlyhab.user',
         'aih_aih.happlyhab.password',
