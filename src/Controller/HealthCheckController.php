d3ad04b22ebd5fec294c48abf19625942ab040d3
benoit <benoit.barberot@alpeshabitat.fr>
2026-01-12 17:02:51 +0100

    📦 NEW: Support symfony 8 (drop symfony 5)

--- src/Controller/HealthCheckController.php
+++ src/Controller/HealthCheckController.php
@@ -6,7 +6,7 @@ namespace Aih\AihBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HealthCheckController extends AbstractController
 {
